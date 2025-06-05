fd82366b373e28c9d9dac0a9eb9d966449f82a72
rollno-07 <vivekrawat0107@gmail.com>
2025-06-05 11:58:56 +0530

    localVariables

--- contracts/localVaribales.sol
+++ contracts/localVaribales.sol
@@ -6,12 +6,15 @@ contract LocalVariables{
      
      uint256 public myNumber;
 
-     function local() public returns (uint256){
+     function local() public returns (address,uint256,uint256){
         //variable defined inside the funtion scope not store on the blockchain
-        uint256 i=234567890;
+        uint256 i=234;
         myNumber=i;
+        i+=45;
 
-        return myNumber;
+        address myaddress=address(1);
+
+        return (myaddress,myNumber,i);
      }
 
 
